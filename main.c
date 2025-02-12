328b119013a9de3af14905fb46fa9e9987508eec
blazejOz <blazej.ozog@gmail.com>
2025-02-12 16:40:21 +0100

    first draft

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define ROWS 30
+#define COLS 50
+
+int grid[ROWS][COLS];
+
+void populateGrid();
+void checkCells();
+void printGrid();
+
+int main()
+{
+
+    return 0;
+}
+
+void populateGrid()
+{
+}
+void checkCells()
+{
+}
+void printGrid()
+{
+}
