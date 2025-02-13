e1247e7e581cde216060abea5ebd2d2c9aa1ebd2
blazejOz <blazej.ozog@gmail.com>
2025-02-13 09:22:33 +0100

    some bugs with the way i implemented aliveNeigbours function...

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define ROWS 30
-#define COLS 50
+#define ROWS 20
+#define COLS 30
 
 int grid[ROWS][COLS] = {0};
 
@@ -15,6 +16,15 @@ int main()
 {
     populateGrid();
     printGrid();
+    checkCells();
+    printGrid();
+
+    // while(1){
+    //     checkCells();
+    //     printGrid();
+    //     sleep(1);
+    // }
+
     return 0;
 }
 
@@ -32,32 +42,75 @@ void populateGrid()
 
 void checkCells()
 {
-    for (int i = 0; i < ROWS; i++)
-    {
-        for (int j = 0; j < COLS; j++)
-        {
-            grid[i][j] = aliveNeighbours(i,j);
+    int tempGrid[ROWS][COLS] = {0};
+
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            tempGrid[i][j] = aliveNeighbours(i,j);
         }
-        putchar('\n');
     }
+
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            grid[i][j] = tempGrid[i][j];
+            tempGrid[i][j] = 0;
+        }
+    }    
+
 }
 
-int aliveNeighbours(int ,int)
-{
 
+/*
+Function checking surrounding cells for alive ones
+(r-1,c-1)|(r-1,c)|(r-1,c+1)
+(r  ,c-1)|(r  ,c)|(r  ,c+1)
+(r+1,c-1)|(r+1,c)|(r+1,c+1)
+
+serching around cell - grid[r][c]
+
+*/
+int aliveNeighbours(int r , int c)
+{
+    int aliveCount = 0;
+    int isAlive = grid[r][c]; //checking if cell is alive or dead
+
+    for (int i = -1; i <= 1; i++){
+        for (int j = -1; j <= 1; j++){
+            if(i == 0 && j == 0) continue; // skips checking itself
+
+            if( (r+i >= 0 && r+i < ROWS) && (c+j >= 0 && c+j < COLS) )
+                if (grid[r+i][c+j] == 1) 
+                    aliveCount++;
+        }
+    }
+
+//Conwey's Game of life Rules
+
+    if(isAlive == 1 && aliveCount < 2 ) 
+        return 0;
+    
+    if(isAlive == 1 && (aliveCount == 2 || aliveCount == 3) )
+        return 1; 
+
+    if(isAlive == 1 && aliveCount > 3 ) 
+        return 0;
+
+    if(isAlive == 0 && aliveCount == 3 )
+        return 1; 
 
 }
 
 void printGrid()
 {
-    for (int i = 0; i < ROWS; i++)
-    {
-        for (int j = 0; j < COLS; j++)
-        {
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            printf("%d ", grid[i][j]);
+            /*
             if (grid[i][j] == 1)
                 printf("\033[47m  ");
             else
                 printf("\033[0m  ");
+            */
         }
         putchar('\n');
     }
